2d17ea0943f1aa1a7727140c93f7b0cacafee265
Gary Passero <gpassero@gmail.com>
2016-08-12 21:44:49 -0400

    Code cleanup. Adding comments

--- RomanNumeral.c
+++ RomanNumeral.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LENGTH 24
+
 /** 
  * The algorithm I use to convert a decimal to a roman numeral 
  * is to look for the biggest single roman symbol that is included
@@ -12,7 +14,6 @@
  * combinations (see below). Thus, as a post-processing step, I replace
  * any instances of those symbol combinations with their correct form.
  */
-
 #define NumberOfIllegalCombinations 6
 const char * IllegalCombinations[NumberOfIllegalCombinations] = {
 	"DCCCC",
@@ -23,6 +24,10 @@ const char * IllegalCombinations[NumberOfIllegalCombinations] = {
 	"IIII",
 };
 
+/**
+ * For a given disallowed character combination, return the preferred
+ * expression with the same value.
+ */
 const char * GetIllegalCombinationReplacement(const char * illegalCombination)
 {
 	const char * replacement = NULL;
@@ -56,30 +61,35 @@ const char * GetIllegalCombinationReplacement(const char * illegalCombination)
 int GetSymbolValue(char symbol)
 {
 	int value = 0;
-	switch (symbol)
+	if (symbol == 'I')
 	{
-		case 'I':
-			value = 1;
-			break;
-		case 'V':
-			value = 5;
-			break;
-		case 'X':
-			value = 10;
-			break;
-		case 'L':
-			value = 50;
-			break;
-		case 'C':
-			value = 100;
-			break;
-		case 'D':
-			value = 500;
-			break;
-		case 'M':
-			value = 1000;
-			break;
+		value = 1;
 	}
+	else if (symbol == 'V')
+	{
+		value = 5;
+	}
+	else if (symbol == 'X')
+	{
+		value = 10;
+	}
+	else if (symbol == 'L')
+	{
+		value = 50;
+	}
+	else if (symbol == 'C')
+	{
+		value = 100;
+	}
+	else if (symbol == 'D')
+	{
+		value = 500;
+	}
+	else if (symbol == 'M')
+	{
+		value = 1000;
+	}
+
 	return value;
 }
 
@@ -131,6 +141,15 @@ void ReplaceAll(char * expr, const char * search, const char * replacement)
 	}
 }
 
+/**
+ * Search through the RomanNumeral expression for illegal symbol combinations.
+ * If any are found, replace with the preferred symbolic expression having the
+ * same value.
+ *
+ * Note that it is necessary to search for the illegal combinations in 
+ * descending value order. That is, we shouldn't replace IIII with IV before
+ * looking for VIIII and replacing it with IX.
+ */
 void ReplaceIllegalCombinations(char * expr)
 {
 	for (int comboIndex = 0; comboIndex < NumberOfIllegalCombinations; comboIndex++)
@@ -141,13 +160,17 @@ void ReplaceIllegalCombinations(char * expr)
 	}
 }
 
+/**
+ * Convert a decimal number to a roman numeral (at most MAX_LENGTH chars). 
+ * The chararacter pointer must be free'd by caller
+ */
 char * DecimalToRoman(int decimal)
 {
 	int remainder = decimal;
 	char symbol = '\0';
 	int index = 0;
 	int symbolicValue = 0;
-	int expressionSize = sizeof(char) * 24;
+	int expressionSize = sizeof(char) * MAX_LENGTH;
 	char * romanNumeralExpr = malloc(expressionSize);
 	while (remainder > 0)
 	{
